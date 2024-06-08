e14336b0191bc2860b91b970482f80e4b54ab4d3
genkokudo <y-toyoda@live.jp>
2024-06-09 01:26:57 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfCT8Sample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCT8Sample.ViewModels
+{
+    internal partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string _testButtonText;
+
+        public MainPageViewModel()
+        {
+            TestButtonText = "Test Button";
+        }
+    }
+}
